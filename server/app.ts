04aaf4b486adcedf7705746f83fecad9bc38e862
swapnil404 <swapnil.christian.developer@gmail.com>
2025-12-13 19:52:04 +0530

    vite and react setup

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
-import { expensesRoute } from "./expenses";
+import { expensesRoute } from "./routes/expenses";
 const app = new Hono();
 
 app.use("*", logger());
