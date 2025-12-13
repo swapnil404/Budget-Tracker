35da930ccf9a1d5f2cef0ee2fb15ffaf1504af02
swapnil404 <swapnil.christian.developer@gmail.com>
2025-12-13 13:45:46 +0530

    tested api using fake expenses

--- expenses.ts
+++ expenses.ts
@@ -2,14 +2,27 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 const expensesRoute = new Hono();
 
-expensesRoute.use("*", logger()); 
+expensesRoute.use("*", logger());
 
-expensesRoute.get("/", (c) => {
-    return c.json({expense:[]});
-})
+type expense = {
+  id: number;
+  title: string;
+  amount: number;
+};
+const fakeExpenses: expense[] = [
+  { id: 1, title: "Groceries", amount: 52.75 },
+  { id: 2, title: "Electricity Bill", amount: 120.0 },
+  { id: 3, title: "Monthly Rent", amount: 1500.0 },
+  { id: 4, title: "Gym Membership", amount: 29.99 },
+  { id: 5, title: "Coffee", amount: 4.5 },
+];
+
+expensesRoute.get("/", async (c) => {
+  return c.json({ expense: fakeExpenses });
+});
 expensesRoute.post("/", async (c) => {
-    const expense = await c.req.json();
-    return c.json({ message: "Expense created", expense }, 201);
+  const expense = await c.req.json();
+  return c.json({ expense });
 });
 
 export { expensesRoute };
